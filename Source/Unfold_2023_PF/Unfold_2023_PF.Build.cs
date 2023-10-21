5a7dad530f0a1cf18a9c25efee5a8502f0d52647
Vishnu <84490319+Cosimarex@users.noreply.github.com>
2023-10-21 08:39:54 +0530

    Pre Adding Backend Data (launched on aws along with multiplayer server)

--- Source/Unfold_2023_PF/Unfold_2023_PF.Build.cs
+++ Source/Unfold_2023_PF/Unfold_2023_PF.Build.cs
@@ -8,7 +8,7 @@ public class Unfold_2023_PF : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HTTP", "Json", "ImageWrapper" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
